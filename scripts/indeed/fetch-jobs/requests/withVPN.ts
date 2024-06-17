fa3165306bfe12d22b1aa881e4c69894bb9b04e6
Banaanas <cyril.developpeur@gmail.com>
2024-06-17 17:50:53 +0200

    feat: add comment

--- scripts/indeed/fetch-jobs/requests/withVPN.ts
+++ scripts/indeed/fetch-jobs/requests/withVPN.ts
@@ -7,6 +7,7 @@ const headers = {
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36",
 };
 
+// We had to use playwright browser - instead of Axios - because Indeed blocked browserless requests
 export const fetchIndeedVPN = async (targetUrl: string): Promise<string> => {
   await verifyVPNUsage();
 

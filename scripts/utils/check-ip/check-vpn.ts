1f09f7873faa7dd0ccf142f7c39be7b969b0f786
Banaanas <cyril.developpeur@gmail.com>
2024-06-18 09:15:47 +0200

    feat: new VPN IP

--- scripts/utils/check-ip/check-vpn.ts
+++ scripts/utils/check-ip/check-vpn.ts
@@ -9,7 +9,7 @@ import colors from "colors";
  * through the ProtonVPN GUI.
  */
 
-const vpnIp = "138.199.22.107"; // Replace with your actual VPN IP address
+const vpnIp = "185.107.56.140"; // Replace with your actual VPN IP address
 
 export const verifyVPNUsage = async () => {
   const ipApi = "https://httpbin.org/ip";

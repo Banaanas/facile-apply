fdfec19dd4fb45634ebad0d3668abdbd3bbf1476
Banaanas <cyril.developpeur@gmail.com>
2024-07-27 09:49:51 +0200

    feat: add phone number handler to contact info handler

--- scripts/indeed/auto-apply-job/url-handlers.ts/pages/contact-info-handler.ts
+++ scripts/indeed/auto-apply-job/url-handlers.ts/pages/contact-info-handler.ts
@@ -2,12 +2,16 @@ import { IndeedJob } from "@prisma/client";
 import { Page } from "playwright";
 
 import { handlePageBasedOnUrl } from "@/scripts/indeed/auto-apply-job/apply-to-job";
+import { cyrilPersonalInfo } from "@/scripts/indeed/auto-apply-job/data/gpt/profile/personal-info";
 
 export const contactInfoHandler = async (
   page: Page,
   indeedJobId: IndeedJob["id"],
 ) => {
   console.log("Handling Contact Info Page");
+
+  // Handle phone number country selection
+  await handlePhoneNumberCountrySelection(page);
 
   // For now, we have to click on the parent because of multiple buttons in one container resulting in an ineffective click
 
@@ -17,4 +21,25 @@ export const contactInfoHandler = async (
   await buttonContainer?.click();
   console.log("Contact Info handled successfully.");
   await handlePageBasedOnUrl(page, indeedJobId);
+};
+
+const handlePhoneNumberCountrySelection = async (page: Page) => {
+  console.log("Checking for phone number country select element.");
+
+  // Check if the select element for phone number country is present
+  const selectElement = await page.$('select[name="phoneNumberCountry"]');
+
+  if (!selectElement) return;
+
+  console.log("Phone number country select element found.");
+
+  // Select the option with value "FR" (France)
+  await selectElement.selectOption("FR");
+  console.log("Selected France (FR) in the phone number country select.");
+
+  // Enter the phone number
+  const phoneNumberInput = await page.$('input[name="phoneNumber"]');
+  if (!phoneNumberInput) return;
+
+  await phoneNumberInput.fill(cyrilPersonalInfo.phone);
 };

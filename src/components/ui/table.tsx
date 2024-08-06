b084f91bf3307b7bab2376fd2cb0737c677b930f
Banaanas <cyril.developpeur@gmail.com>
2024-08-06 18:37:30 +0200

    refactor: remove useless class

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -78,7 +78,7 @@ const TableHead = forwardRef<
     <th
       ref={ref}
       className={cn(
-        "h-12 px-4 text-left align-middle bg- font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
+        "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
         className,
       )}
       {...props}

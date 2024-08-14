04aec72196b71932cd45240397610932135812a2
Banaanas <cyril.developpeur@gmail.com>
2024-08-14 19:49:55 +0200

    Rename script, refactor Docker setup, add npm commands
    Renamed and moved the Docker setup script for better organization. The script now starts the Postgres container without switching directories or running npm tasks directly. Updated `package.json` with new npm script commands for running Docker and job fetching processes.

--- scripts/bash/run_docker.sh
+++ scripts/bash/run_docker.sh
@@ -13,22 +13,22 @@ print_error() {
 # Check if Docker daemon is running
 if ! docker info > /dev/null 2>&1; then
   print_message "Docker daemon is not running. Attempting to start Docker..."
-  
+
   # Start Docker for macOS
   open --background -a Docker
-  
+
   # Wait until Docker daemon is running
   while ! docker info > /dev/null 2>&1; do
     print_message "Waiting for Docker to start..."
     sleep 1
   done
-  
+
   print_message "Docker daemon started successfully."
 else
   print_message "Docker daemon is already running."
 fi
 
-# Run your Docker command
+# Start the Postgres container if it isn't already running
 if docker ps -q -f name=facile-apply-postgres-db | grep -q .; then
   print_message "Postgres container is already running."
 else
@@ -39,26 +39,4 @@ else
     print_error "Failed to start the Postgres container."
     exit 1
   fi
-fi
-
-# Change to the project directory
-cd /Users/cyril/Desktop/facile-apply || {
-  print_error "Failed to change directory to /Users/cyril/Desktop/facile-apply."
-  exit 1
-}
-
-# Run the indeed-jobs npm script
-if npm run indeed-jobs; then
-  print_message "NPM script indeed-jobs ran successfully."
-else
-  print_error "NPM script indeed-jobs failed."
-  exit 1
-fi
-
-# Run the linkedin-jobs npm script
-if npm run linkedin-jobs; then
-  print_message "NPM script linkedin-jobs ran successfully."
-else
-  print_error "NPM script linkedin-jobs failed."
-  exit 1
 fi

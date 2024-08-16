362f03233822b98477a6f30ea396f6aefe6d64fb
Banaanas <cyril.developpeur@gmail.com>
2024-08-16 18:35:07 +0200

    Log VPN usage advice and enhance anti-bot detection
    Added a suggestion to change the VPN IP address if anti-bot protection persists. Improved challenge page detection by including "Verification Required" to better handle varied challenge scenarios.

--- scripts/indeed/fetch-jobs/requests/prevent-challenge.ts
+++ scripts/indeed/fetch-jobs/requests/prevent-challenge.ts
@@ -13,6 +13,11 @@ export const fetchWithRetry = async (indeedSearchUrl: string) => {
         "Indeed's anti-bot protection seems to be active. Retrying fetch... ",
       ),
     );
+    console.log(
+      chalk.bgRed(
+        "If this persist and you are using a VPN, please change the IP you're fetching with. ",
+      ),
+    );
     // Wait for a certain period before re-fetch
     await new Promise((resolve) => {
       setTimeout(resolve, 500);
@@ -28,6 +33,7 @@ export const fetchWithRetry = async (indeedSearchUrl: string) => {
 const isChallengePage = (htmlContent: string) => {
   return (
     htmlContent.includes("Enable JavaScript and cookies to continue") ||
-    htmlContent.includes("Just a moment...")
+    htmlContent.includes("Just a moment...") ||
+    htmlContent.includes("Verification Required")
   );
 };
